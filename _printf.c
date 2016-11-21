65d7809c77adbc8acf7a3bbb31ff45e72608a552
randomvs <victorearellano@gmail.com>
2016-11-21 19:52:38 +0000

    correcting functions

---
+++ _printf.c
@@ -0,0 +1,16 @@
+#include
+
+
+void _printf(unsigned int parameters, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *s;
+
+	va_start(ap, parameters)
+		for (i = 0; i < n; i ++)
+		{
+			va_arg(ap, XXX);
+		}
+	va_end(ap);
+}
