0c3ed6da2eb2b26f00f5028288955bf947af8fd2
randomvs <victorearellano@gmail.com>
2016-11-21 17:41:40 +0000

    initialize _printf()

---
+++ holberton.h
@@ -0,0 +1,5 @@
+#ifndef _PRINT_FUNCTION_H_
+#define _PRINT_FUNCTION_H_
+
+
+#endit
---
+++ p_op.c
@@ -0,0 +1,31 @@
+#ifndef _PRINT_FLAGS_H_
+#define _PRINT_FLAGS_H_
+
+
+void p_int()
+{
+
+}
+
+
+void p_char()
+{
+
+
+}
+
+void p_float()
+{
+
+}
+
+void p_string()
+{
+
+}
+
+
+
+
+
+#endif
